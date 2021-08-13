ef0689ec4e534261545adeae3f43295b96622f56
neuecc <ils@neue.cc>
2021-08-13 18:35:45 +0900

    WPF

--- sandbox/WpfApp/MainWindow.xaml.cs
+++ sandbox/WpfApp/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ObservableCollections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,33 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableList<int> list;
+        public ISynchronizedView<int, int> ItemsView { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+
+
+
+            list = new ObservableList<int>();
+            list.AddRange(new[] { 1, 10, 188 });
+            ItemsView = list.CreateSortedView(x => x, x => x, comparer: Comparer<int>.Default).WithINotifyCollectionChanged();
+
+
+            BindingOperations.EnableCollectionSynchronization(ItemsView, new object());
+        }
+
+        int adder = 99;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                list.Add(adder++);
+            });
         }
     }
 }
