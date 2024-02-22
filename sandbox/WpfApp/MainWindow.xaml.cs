170eecf922fa20e867830892888a901c85ac0c6b
neuecc <ils@neue.cc>
2024-02-22 18:39:06 +0900

    WPF Check

--- sandbox/WpfApp/MainWindow.xaml.cs
+++ sandbox/WpfApp/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using ObservableCollections;
+using R3;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -22,38 +24,72 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableList<int> list;
-        public INotifyCollectionChangedSynchronizedView<int> ItemsView { get; set; }
+        //ObservableList<int> list;
+        //public INotifyCollectionChangedSynchronizedView<int> ItemsView { get; set; }
+
+
+
 
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = this;
+
+
+            R3.WpfProviderInitializer.SetDefaultObservableSystem(x =>
+            {
+                Trace.WriteLine(x);
+            });
+
+            this.DataContext = new ViewModel();
 
 
 
 
-            list = new ObservableList<int>();
-            list.AddRange(new[] { 1, 10, 188 });
-            ItemsView = list.CreateSortedView(x => x, x => x, comparer: Comparer<int>.Default).ToNotifyCollectionChanged();
+            //list = new ObservableList<int>();
+            //list.AddRange(new[] { 1, 10, 188 });
+            //ItemsView = list.CreateSortedView(x => x, x => x, comparer: Comparer<int>.Default).ToNotifyCollectionChanged();
 
+
+            //BindingOperations.EnableCollectionSynchronization(ItemsView, new object());
+        }
+
+        //int adder = 99;
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ThreadPool.QueueUserWorkItem(_ =>
+        //    {
+        //        list.Add(adder++);
+        //    });
+        //}
+
+        //protected override void OnClosed(EventArgs e)
+        //{
+        //    ItemsView.Dispose();
+        //}
+    }
+
+    public class ViewModel
+    {
+        private ObservableList<int> observableList { get; } = new ObservableList<int>();
+        public INotifyCollectionChangedSynchronizedView<int> ItemsView { get; }
+        public ReactiveCommand<Unit> ClearCommand { get; } = new ReactiveCommand<Unit>();
+
+        public ViewModel()
+        {
+            observableList.Add(1);
+            observableList.Add(2);
+
+            ItemsView = observableList.CreateView(x => x).ToNotifyCollectionChanged();
 
             BindingOperations.EnableCollectionSynchronization(ItemsView, new object());
-        }
 
-        int adder = 99;
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            ThreadPool.QueueUserWorkItem(_ =>
+            // var iii = 10;
+            ClearCommand.Subscribe(_ =>
             {
-                list.Add(adder++);
+                // observableList.Add(iii++);
+                observableList.Clear();
             });
-        }
-
-        protected override void OnClosed(EventArgs e)
-        {
-            ItemsView.Dispose();
         }
     }
 }
