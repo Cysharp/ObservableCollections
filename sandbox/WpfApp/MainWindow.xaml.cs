e7e914501155b7a53524cf715b7fdbba83b8752e
zerodev1200 <42404360+zerodev1200@users.noreply.github.com>
2024-10-17 23:28:25 +0900

    add four DataGrid in sandbox-WpfApp

--- sandbox/WpfApp/MainWindow.xaml.cs
+++ sandbox/WpfApp/MainWindow.xaml.cs
@@ -87,6 +87,18 @@ namespace WpfApp
         public ReactiveCommand<Unit> AttachFilterCommand { get; } = new ReactiveCommand<Unit>();
         public ReactiveCommand<Unit> ResetFilterCommand { get; } = new ReactiveCommand<Unit>();
 
+        private ObservableList<Person> SourceList { get; } = [];
+        private IWritableSynchronizedView<Person, Person> writableFilter;
+        private ISynchronizedView<Person, Person> notWritableFilter;
+        public NotifyCollectionChangedSynchronizedViewList<Person> NotWritableNonFilterView { get; }
+        public NotifyCollectionChangedSynchronizedViewList<Person> NotWritableFilterView { get; }
+        public NotifyCollectionChangedSynchronizedViewList<Person> WritableNonFilterPersonView { get; }
+        public NotifyCollectionChangedSynchronizedViewList<Person> WritableFilterPersonView { get; }
+        public ReactiveCommand<Unit> AttachFilterCommand2 { get; } = new ReactiveCommand<Unit>();
+        public ReactiveCommand<Unit> ResetFilterCommand2 { get; } = new ReactiveCommand<Unit>();
+        public ReactiveCommand<Unit> AttachFilterCommand3 { get; } = new ReactiveCommand<Unit>();
+        public ReactiveCommand<Unit> ResetFilterCommand3 { get; } = new ReactiveCommand<Unit>();
+
         public ViewModel()
         {
             observableList.Add(1);
@@ -156,7 +168,96 @@ namespace WpfApp
             {
                 view.ResetFilter();
             });
+
+            SourceList.Add(new() { Name = "a", Age = 1 });
+            SourceList.Add(new() { Name = "b", Age = 2 });
+            SourceList.Add(new() { Name = "c", Age = 3 });
+            SourceList.Add(new() { Name = "d", Age = 4 });
+            //NotWritable, NonFilter
+            NotWritableNonFilterView = SourceList.ToNotifyCollectionChanged();
+
+            //NotWritable, Filter
+            notWritableFilter = SourceList.CreateView(x => x);
+            NotWritableFilterView = notWritableFilter.ToNotifyCollectionChanged();
+
+            //Writable, NonFilter
+            WritableNonFilterPersonView = SourceList.ToWritableNotifyCollectionChanged();
+
+            //WritableNonFilterPersonView = SourceList.ToWritableNotifyCollectionChanged(x => x, (Person newView, Person original, ref bool setValue) =>
+            //{
+            //    if (setValue)
+            //    {
+            //        // default setValue == true is Set operation
+            //        original.Name = newView.Name;
+            //        original.Age = newView.Age;
+
+            //        // You can modify setValue to false, it does not set original collection to new value.
+            //        // For mutable reference types, when there is only a single,
+            //        // bound View and to avoid recreating the View, setting false is effective.
+            //        // Otherwise, keeping it true will set the value in the original collection as well,
+            //        // and change notifications will be sent to lower-level Views(the delegate for View generation will also be called anew).
+            //        setValue = false;
+            //        return original;
+            //    }
+            //    else
+            //    {
+            //        // default setValue == false is Add operation
+            //        return new Person { Age = newView.Age, Name = newView.Name };
+            //    }
+            //}, null);
+
+            //Writable, Filter
+            writableFilter = SourceList.CreateWritableView(x => x);
+            WritableFilterPersonView = writableFilter.ToWritableNotifyCollectionChanged();
+
+            //WritableFilterPersonView = writableFilter.ToWritableNotifyCollectionChanged((Person newView, Person original, ref bool setValue) =>
+            //{
+            //    if (setValue)
+            //    {
+            //        // default setValue == true is Set operation
+            //        original.Name = newView.Name;
+            //        original.Age = newView.Age;
+
+            //        // You can modify setValue to false, it does not set original collection to new value.
+            //        // For mutable reference types, when there is only a single,
+            //        // bound View and to avoid recreating the View, setting false is effective.
+            //        // Otherwise, keeping it true will set the value in the original collection as well,
+            //        // and change notifications will be sent to lower-level Views(the delegate for View generation will also be called anew).
+            //        setValue = false;
+            //        return original;
+            //    }
+            //    else
+            //    {
+            //        // default setValue == false is Add operation
+            //        return new Person { Age = newView.Age, Name = newView.Name };
+            //    }
+            //});
+
+            AttachFilterCommand2.Subscribe(_ =>
+            {
+                notWritableFilter.AttachFilter(x => x.Age % 2 == 0);
+            });
+
+            ResetFilterCommand2.Subscribe(_ =>
+            {
+                notWritableFilter.ResetFilter();
+            });
+
+            AttachFilterCommand3.Subscribe(_ =>
+            {
+                writableFilter.AttachFilter(x => x.Age % 2 == 0);
+            });
+
+            ResetFilterCommand3.Subscribe(_ =>
+            {
+                writableFilter.ResetFilter();
+            });
         }
+    }
+    public class Person
+    {
+        public int? Age { get; set; }
+        public string? Name { get; set; }
     }
 
     public class WpfDispatcherCollection(Dispatcher dispatcher) : ICollectionEventDispatcher
